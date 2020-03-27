adceafc8fb492c73f2b5377e311b2941f1d52ff9
Chamber36 <59872875+Mwenge1988@users.noreply.github.com>
2020-03-27 00:14:02 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    Add: function(x,y){
+        return x+y;
+    },
+    Subtract: function(x,y){
+        return x-y;
+    }
+};
---
+++ comparer.js
@@ -0,0 +1,11 @@
+exports.AreNumberEqual = function (x,y) {
+    if (x === y) {
+        console.log('numbers are equal!');
+        return true;
+        
+    } else {
+        console.log('these numbers are not equal!');
+        return false;
+    }
+
+};
